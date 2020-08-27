ba54c9bc993377a38797582180703fb45fdf80ba
Petrusiova <Olaola1221>
2020-08-27 23:21:14 +0300

    - deleted useless Exceptions

--- src/main/java/graduation/graduationProject/util/exception/IllegalRequestDataException.java
+++
@@ -1,7 +0,0 @@
-package graduation.graduationProject.util.exception;
-
-public class IllegalRequestDataException extends RuntimeException {
-    public IllegalRequestDataException(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/graduation/graduationProject/web/ExceptionInfoHandler.java
+++ src/main/java/graduation/graduationProject/web/ExceptionInfoHandler.java
@@ -4,23 +4,18 @@ import graduation.graduationProject.util.ValidationUtil;
 import graduation.graduationProject.util.exception.ApplicationException;
 import graduation.graduationProject.util.exception.ErrorInfo;
 import graduation.graduationProject.util.exception.ErrorType;
-import graduation.graduationProject.util.exception.IllegalRequestDataException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -65,20 +60,7 @@ public class ExceptionInfoHandler {
     @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)  // 422
     @ExceptionHandler({BindException.class, MethodArgumentNotValidException.class})
     public ErrorInfo bindValidationError(HttpServletRequest req, Exception e) {
-        BindingResult result = e instanceof BindException ?
-                ((BindException) e).getBindingResult() : ((MethodArgumentNotValidException) e).getBindingResult();
-
-//        String[] details = result.getFieldErrors().stream()
-//                .map(messageSourceAccessor::getMessage)
-//                .toArray(String[]::new);
-
         return logAndGetErrorInfo(req, e, false, VALIDATION_ERROR, e.getMessage());
-    }
-
-    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)  // 422
-    @ExceptionHandler({IllegalRequestDataException.class, MethodArgumentTypeMismatchException.class, HttpMessageNotReadableException.class})
-    public ErrorInfo illegalRequestDataError(HttpServletRequest req, Exception e) {
-        return logAndGetErrorInfo(req, e, false, VALIDATION_ERROR);
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
